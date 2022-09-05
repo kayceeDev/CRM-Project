f5d275cc6921bef0be07b46f03ba5c819929aff7
Ekene Nwobodo <nwobodoe71@gmail.com>
2022-09-05 11:54:31 +0100

    Add util files

---
+++ utils/checkFile.go
@@ -0,0 +1,17 @@
+package utils
+
+import(
+	"os"
+)
+
+
+func CheckFile(filename string) (error) {
+	_, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		_, err := os.Create(filename)
+			if err != nil {
+				 return err
+			}
+	}
+	return nil
+}
